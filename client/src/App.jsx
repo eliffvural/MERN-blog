c57d1b7618422bb501452a9a55a915157939ab40
Elif Vural <elifvural019@gmail.com>
2024-07-20 16:42:33 +0300

    "Removed React import and added text color to h1 element"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
 export default function App() {
   return (
-      <h1  className='text-3xl' >MERN blog</h1>
+     <h1 className='text-3xl text-red-500'>MERN blog</h1>
   )
 }
