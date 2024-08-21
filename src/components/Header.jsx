09bf573eb994712470dc9f72ad469bc78c838a27
Elif Vural <elifvural019@gmail.com>
2024-08-21 22:47:25 +0300

    "Updated Button colors and added text to Sign In button in Header component"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
     
           />
         </form>
-        <Button  className='w-12 h-10 lg:hidden' color='orange' pill>
+        <Button  className='w-12 h-10 lg:hidden' color='gray' pill>
           <AiOutlineSearch/>
         </Button>
         <div className="flex items-center space-x-4">
@@ -33,8 +33,8 @@ export default function Header() {
             <FaMoon />
           </Button>
           <Link to="/sign-in">
-          <Button className="w-12 h-10 hidden sm:inline">
-
+          <Button className="w-12 h-10 hidden sm:inline" color='gray' pill>
+               Sign In
           </Button>
           
           </Link>
