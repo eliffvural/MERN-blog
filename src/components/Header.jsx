de7f77326406b3dedd5879d95055f36b5603a31f
Elif Vural <elifvural019@gmail.com>
2024-08-21 23:24:33 +0300

    "Refactor Header component: updated imports, rearranged layout, and added conditional active state to Navbar links"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,63 +1,57 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Button, Navbar, NavbarToggle, TextInput } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const path= useLocation().pathname;
   return (
-    
-      <Navbar className="border-b-2">
-        <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl
-        font-semibold dark:text-white">
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Hijjj
-        </span>
-        Blog
+    <Navbar className="border-b-2">
+      <div className="flex justify-between w-full">
+        <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Hijjj
+          </span>
+          Blog
         </Link>
-        <form>
+        
+        <form className="flex-grow mx-4 hidden lg:block">
           <TextInput 
-          
-          type="text"
-          placeholder="Search..."
-          rightIcon={AiOutlineSearch}
-          className="hidden lg:inline"
-    
+            type="text"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+            className="w-full"
           />
         </form>
-        <Button  className='w-12 h-10 lg:hidden' color='gray' pill>
-          <AiOutlineSearch/>
-        </Button>
-        <div className="flex gap-2 md:order-2">
+        
+        <div className="flex items-center gap-2 md:order-2">
           <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
             <FaMoon />
           </Button>
           <Link to="/sign-in">
-          <Button gradientDuoTone='orangeToPurple'>
-               Sign In
-          </Button>
+            <Button gradientDuoTone='orangeToPurple'>
+              Sign In
+            </Button>
           </Link>
-
-          <Navbar.Collapse>
-            <Navbar.Link>
-              <Link to='/'>
-               Home
-              </Link>
-            </Navbar.Link>
-            <Navbar.Link>
-              <Link to='/about'>
-              About
-              </Link>
-            </Navbar.Link>
-            <Navbar.Link>
-              <Link to='/projects'>
-              Projects
-              </Link>
-            </Navbar.Link>
-          </Navbar.Collapse>
-
-
+          <NavbarToggle/>
+          <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+            <AiOutlineSearch/>
+          </Button>
+          
         </div>
-        </Navbar>
-
-  )
+      </div>
+      
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
