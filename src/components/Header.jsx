2d529225520d492982d7a5af82a82f9b386d95b5
Elif Vural <elifvural019@gmail.com>
2024-08-21 21:32:36 +0300

    "Added Link component from react-router-dom to Header component"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import { Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     
-      <Navbar className="border-b-2">Header</Navbar>
+      <Navbar className="border-b-2">
+        <Link to="/">
+
+
+        </Link>
+        </Navbar>
 
   )
 }
