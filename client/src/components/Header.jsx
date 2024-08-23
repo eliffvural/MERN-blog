0bd6ea596f0db53c9d690ff35c42f2b72953eef9
Elif Vural <elifvural019@gmail.com>
2024-08-23 09:55:20 +0300

    Remove empty lines and fix indentation in Header component.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,10 +35,8 @@ export default function Header() {
           pill>
         </Button>
         
-     
-       
           <Link to='/sign-in'>
-            <Button className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+            <Button className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white' >
               Sign In
             </Button>
           </Link>
