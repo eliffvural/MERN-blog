279c8014aa35ba39e2474c370e25ae3b87d1b884
Elif Vural <elifvural019@gmail.com>
2024-08-25 01:17:48 +0300

    "Added MongoDB connection success logging"

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 
-mongoose.connect('mongodb+srv://elifvural03:elifvural03@mern-blog.el1cf.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog');
+mongoose.connect('mongodb+srv://elifvural03:elifvural03@mern-blog.el1cf.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog')
+
+.then(()=> {
+    console.log("Connected to MongoDB");
+});
 
 const app = express();
 
