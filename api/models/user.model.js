5d901c2454cbe03ebc5dd0462aed2ca9eff30070
Elif Vural <elifvural019@gmail.com>
2024-08-26 16:47:28 +0300

    Added 'email' field to userSchema in user.model.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -8,6 +8,13 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+        },
     
+    
+
 
 })
