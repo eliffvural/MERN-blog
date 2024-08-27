b04ac70a4d2e0d249e56b6d1e8cb88e83a1ff601
Elif Vural <elifvural019@gmail.com>
2024-08-27 20:12:34 +0300

    "Added auth routes and imported auth controller"

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 
 dotenv.config();
@@ -21,3 +22,4 @@ app.listen(3000, ()=>{
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
