3e0ba34a6c86baa06e6775b63b42da28320fcfe3
Elif Vural <elifvural019@gmail.com>
2024-08-30 17:10:34 +0300

    "Added error response formatting in global error handler"

--- api/index.js
+++ api/index.js
@@ -31,4 +31,10 @@ app.use((err, req,res, next) => {
     const statusCode = err.statusCode || 500;
     const message= err.message || 'internal server error!!';
 
-})
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+
+});
