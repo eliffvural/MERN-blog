9075f329b23765cfcf129c42565427e3522fcdc2
Elif Vural <elifvural019@gmail.com>
2024-09-07 15:37:59 +0300

    "Updated Footer component with new HTML structure and changed border color to teal-500"

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,16 @@
-import { Footer } from "flowbite-react"
+import { Footer } from "flowbite-react";
 
 export default function FooterCom() {
   return (
-    <Footer container className="border border-t-8 border-t-neutral-500 ">
-      Footer
+    <Footer container className="border border-t-8 border-teal-500 ">
+      <div className="">
+        <div className="">
+            <div className="">
+
+            </div>
+
+        </div>
+      </div>
     </Footer>
-  )
+  );
 }
